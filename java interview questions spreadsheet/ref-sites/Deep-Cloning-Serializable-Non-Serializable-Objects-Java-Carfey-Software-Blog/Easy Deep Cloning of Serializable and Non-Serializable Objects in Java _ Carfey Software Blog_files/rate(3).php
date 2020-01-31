1f4ca3aedbefb55ad208b76ef688073554947dd6
at kuzmanov <atkuzmanov@gmail.com>
2020-01-31 15:51:32 +0200

    WIP

---
+++ java interview questions spreadsheet/ref-sites/Deep-Cloning-Serializable-Non-Serializable-Objects-Java-Carfey-Software-Blog/Easy Deep Cloning of Serializable and Non-Serializable Objects in Java _ Carfey Software Blog_files/rate(3).php
@@ -0,0 +1,6 @@
+PDRTJS_3724155_comm_64563.nero_up = 0;
+PDRTJS_3724155_comm_64563.nero_dn = 0;
+PDRTJS_settings_3724155_comm_64563= {			"type" : "nero",			"size" : "sml",			"star_color" : "hand",			"custom_star" : "",			"font_size" : "",			"font_line_height" : "16px",			"font_color" : "",			"font_align" : "left",			"font_position" : "right",			"font_family" : "",			"font_bold" : "normal",			"font_italic" : "normal",			"text_vote" : "Vote",			"text_votes" : "Votes",			"text_rate_this" : "Rate This",			"text_1_star" : "Very Poor",			"text_2_star" : "Poor",			"text_3_star" : "Average",			"text_4_star" : "Good",			"text_5_star" : "Excellent",			"text_thank_you" : "Thank You",			"text_rate_up" : "Rate Up",			"text_rate_down" : "Rate Down"		};
+PDRTJS_3724155_comm_64563.init();
+PDRTJS_3724155_comm_64563.token='37fc4f20d10d597678f1e264ced83308';
+/*3724155,_comm_64563,wp-comment-64563,772439275,0-0*/
