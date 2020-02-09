f9fcb939db5e52f4e3d58b0daf38bc0b49f22a4e
at kuzmanov <atkuzmanov@gmail.com>
2020-02-09 14:45:01 +0200

    WIP
    Former-commit-id: f125e06a866d51b62b2efff55bc43a0300c7bb1a

---
+++ java interview questions spreadsheet/refs/Java-Thread-Starvation-Example/Java - Thread Starvation Example_files/ads.js
@@ -0,0 +1,10 @@
+var canRunAds = true;function evalAds(){}
+function doEvalAds(){}
+function sendAjaxAdsEvalRequest(){}
+function sendAjaxRefreshRequest(){}
+function printObject(){}
+function info(){}
+function log(){}
+function toggleLinkCheckbox(){}
+function applyDevToolAction(){}
+function ajaxPostRequest(){}
