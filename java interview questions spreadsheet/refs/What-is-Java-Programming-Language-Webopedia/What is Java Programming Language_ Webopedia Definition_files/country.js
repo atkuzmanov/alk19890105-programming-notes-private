1285bc5be322d8dae86f2730eb7865b21181b342
at kuzmanov <atkuzmanov@gmail.com>
2020-02-18 17:53:33 +0200

    WIP

---
+++ java interview questions spreadsheet/refs/What-is-Java-Programming-Language-Webopedia/What is Java Programming Language_ Webopedia Definition_files/country.js
@@ -0,0 +1,12 @@
+(function () {
+	var country = {'code':'bg','id':30,'defaultLangauge':'bg'};
+	
+	if (!window.evidon) window.evidon = {};
+	
+	if (window.evidon.notice) {
+		window.evidon.notice.setLocation(country);
+	}
+	else {
+		window.evidon.location = country;
+	}
+})();
