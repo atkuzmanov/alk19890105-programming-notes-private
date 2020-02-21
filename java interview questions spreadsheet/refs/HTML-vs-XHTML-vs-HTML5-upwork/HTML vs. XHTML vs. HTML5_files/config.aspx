f7fd60e524741b68560f41e3c4a46a9dabaa86a3
at kuzmanov <atkuzmanov@gmail.com>
2020-02-21 13:03:42 +0200

    WIP
    Former-commit-id: e9e2bcb150a85637b2b6e7c1e4322eba09153123

---
+++ java interview questions spreadsheet/refs/HTML-vs-XHTML-vs-HTML5-upwork/HTML vs. XHTML vs. HTML5_files/config.aspx
@@ -0,0 +1,3 @@
+//<![CDATA[
+try {sessionCamRecorder.initialise({"H":"0k55jmgysrqzidiqksirzj3p","I":false,"AE":1});} catch(e) {sessionCamRecorder.log('init failed (' + e + ')');}
+//]]>
---
+++ java interview questions spreadsheet/refs/HTML-vs-XHTML-vs-HTML5-upwork/HTML vs. XHTML vs. HTML5_files/dsG.js
@@ -0,0 +1,8 @@
+// Qualaroo for upwork.com
+// (C) 2019 Qualaroo. All rights reserved.
+// qualaroo.com
+
+//$ site: 55082, generated: 2019-05-09 11:08:33 UTC
+//$ client: 2.0.55
+
+!function(){var e=document,t=e.getElementsByTagName("script")[0],r=e.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cl.qualaroo.com/ki.js/58403/dsGqoo.js",t.parentNode.insertBefore(r,t)}();
